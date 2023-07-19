a848449b1d6049085b5855d5bdb97487e2007bc5
CoderPoorva <adhikarypoorvaraja@gmail.com>
2023-07-19 21:52:07 +0530

    HOME-POORVA UPDATED

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu= document.querySelector(".icons");
-let nav= document.querySelector(".navbar");
-
-menu.onclick=()=>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
